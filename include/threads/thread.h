c38bafc583628e8713ae751a472b5e76e3735781
GEUMDOHYEON <154233397+GEUMDOHYEON@users.noreply.github.com>
2024-03-04 21:57:16 +0900

    Revert "Alarm Clock middle commit"

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -91,7 +91,6 @@ struct thread {
 	enum thread_status status;          /* Thread state. */
 	char name[16];                      /* Name (for debugging purposes). */
 	int priority;                       /* Priority. */
-	int64_t r_ticks; 						/* local tick */
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
@@ -133,8 +132,7 @@ const char *thread_name (void);
 
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
-void thread_sleep(int64_t);
-void wake_up(void);
+
 int thread_get_priority (void);
 void thread_set_priority (int);
 
